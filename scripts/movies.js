2d25f7f5781f22fe98a3a2ac66abd670bea196a1
Le Trung Hai <hozymary@gmail.com>
2022-04-16 10:39:42 +0800

    Prettier

--- scripts/movies.js
+++ scripts/movies.js
@@ -1,124 +1,124 @@
 function getMovies() {
-    return [
-        {
-            "id": 1,
-            "sources": {
-                "mp4": "https://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
-                "ogg": "https://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.ogg"
-            },
-            "thumb": "/images/casablanca.png",
-            "type": "now",
-            "info": {
-                "title": "Casablanca",
-                "cast": "Humphrey Bogart, Ingrid Bergman, Paul Henreid",
-                "director": "Michael Curtiz",
-                "duration": 102
-            }
-        },
-        {
-            "id": 2,
-            "sources": {
-                "mp4": "https://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
-                "ogg": "https://courses.cs.cityu.edu.hk/cs2204/video/barbecue.ogg"
-            },
-            "thumb": "/images/barbecue.jpg",
-            "type": "now",
-            "info": {
-                "title": "Barbecue",
-                "cast": "Lwazi Dlaminilf, Rufo Servado, Katsunori Yashima",
-                "director": "Matthew Salleh",
-                "duration": 102
-            }
-        },
-        {
-            "id": 3,
-            "sources": {
-                "mp4": "https://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
-                "ogg": "https://courses.cs.cityu.edu.hk/cs2204/video/nature.ogg"
-            },
-            "thumb": "/images/nature.jpg",
-            "type": "now",
-            "info": {
-                "title": "Earth",
-                "cast": "Patrick Stewart",
-                "director": "Alastair Fothergill",
-                "duration": 99
-            }
-        },
-        {
-            "id": 4,
-            "sources": {
-                "mp4": "https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
-                "ogg": "https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.ogg"
-            },
-            "thumb": "/images/bigbuck.jpg",
-            "type": "upcoming",
-            "info": {
-                "title": "Big Buck Bunny",
-                "cast": "Bunny",
-                "director": "Sacha Goedegebure",
-                "duration": 10
-            }
-        },
-        {
-            "id": 5,
-            "sources": {
-                "mp4": "https://courses.cs.cityu.edu.hk/cs2204/video/cakemaking-s.mp4",
-                "ogg": "https://courses.cs.cityu.edu.hk/cs2204/video/cakemaking-s.ogg"
-            },
-            "thumb": "/images/mochi.jpg",
-            "type": "upcoming",
-            "info": {
-                "title": "The Way Back to Yarasquin",
-                "cast": "Mayra Orallana-Powell",
-                "director": "Sarah Gerber",
-                "duration": 35
-            }
-        },
-        {
-            "id": 6,
-            "sources": {
-                "mp4": "https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
-                "ogg": "https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.ogg"
-            },
-            "thumb": "/images/chocolate.jpg",
-            "type": "upcoming",
-            "info": {
-                "title": "Charlie and the Chocolate Factory",
-                "cast": "Johnny Depp, Freddie Highmore",
-                "director": "Tim Burton",
-                "duration": 115
-            }
-        },
-        {
-            "id": 7,
-            "sources": {
-                "mp4": "https://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
-                "ogg": "https://courses.cs.cityu.edu.hk/cs2204/video/wildlife.ogg"
-            },
-            "thumb": "/images/wildlife.jpg",
-            "type": "upcoming",
-            "info": {
-                "title": "Wild Horses",
-                "cast": "Mick Jagger, Brian Jones, Keith Richards, Bill Wyman",
-                "director": "Jimmy Miller",
-                "duration": 46
-            }
-        },
-        {
-            "id": 8,
-            "sources": {
-                "mp4": "https://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
-                "ogg": "https://courses.cs.cityu.edu.hk/cs2204/video/wonders.ogg"
-            },
-            "thumb": "/images/wonders.jpg",
-            "type": "upcoming",
-            "info": {
-                "title": "Mad Max: Fury Road",
-                "cast": "Tom Hardy, Charlize Theron, Nicholas Hoult",
-                "director": "George Miller",
-                "duration": 120
-            }
-        }
-    ]
+  return [
+    {
+      id: 1,
+      sources: {
+        mp4: "https://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.mp4",
+        ogg: "https://courses.cs.cityu.edu.hk/cs2204/video/casablanca-s.ogg",
+      },
+      thumb: "/images/casablanca.png",
+      type: "now",
+      info: {
+        title: "Casablanca",
+        cast: "Humphrey Bogart, Ingrid Bergman, Paul Henreid",
+        director: "Michael Curtiz",
+        duration: 102,
+      },
+    },
+    {
+      id: 2,
+      sources: {
+        mp4: "https://courses.cs.cityu.edu.hk/cs2204/video/barbecue.mp4",
+        ogg: "https://courses.cs.cityu.edu.hk/cs2204/video/barbecue.ogg",
+      },
+      thumb: "/images/barbecue.jpg",
+      type: "now",
+      info: {
+        title: "Barbecue",
+        cast: "Lwazi Dlaminilf, Rufo Servado, Katsunori Yashima",
+        director: "Matthew Salleh",
+        duration: 102,
+      },
+    },
+    {
+      id: 3,
+      sources: {
+        mp4: "https://courses.cs.cityu.edu.hk/cs2204/video/nature.mp4",
+        ogg: "https://courses.cs.cityu.edu.hk/cs2204/video/nature.ogg",
+      },
+      thumb: "/images/nature.jpg",
+      type: "now",
+      info: {
+        title: "Earth",
+        cast: "Patrick Stewart",
+        director: "Alastair Fothergill",
+        duration: 99,
+      },
+    },
+    {
+      id: 4,
+      sources: {
+        mp4: "https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.mp4",
+        ogg: "https://courses.cs.cityu.edu.hk/cs2204/video/bigbuck-s.ogg",
+      },
+      thumb: "/images/bigbuck.jpg",
+      type: "upcoming",
+      info: {
+        title: "Big Buck Bunny",
+        cast: "Bunny",
+        director: "Sacha Goedegebure",
+        duration: 10,
+      },
+    },
+    {
+      id: 5,
+      sources: {
+        mp4: "https://courses.cs.cityu.edu.hk/cs2204/video/cakemaking-s.mp4",
+        ogg: "https://courses.cs.cityu.edu.hk/cs2204/video/cakemaking-s.ogg",
+      },
+      thumb: "/images/mochi.jpg",
+      type: "upcoming",
+      info: {
+        title: "The Way Back to Yarasquin",
+        cast: "Mayra Orallana-Powell",
+        director: "Sarah Gerber",
+        duration: 35,
+      },
+    },
+    {
+      id: 6,
+      sources: {
+        mp4: "https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.mp4",
+        ogg: "https://courses.cs.cityu.edu.hk/cs2204/video/chocolate.ogg",
+      },
+      thumb: "/images/chocolate.jpg",
+      type: "upcoming",
+      info: {
+        title: "Charlie and the Chocolate Factory",
+        cast: "Johnny Depp, Freddie Highmore",
+        director: "Tim Burton",
+        duration: 115,
+      },
+    },
+    {
+      id: 7,
+      sources: {
+        mp4: "https://courses.cs.cityu.edu.hk/cs2204/video/wildlife.mp4",
+        ogg: "https://courses.cs.cityu.edu.hk/cs2204/video/wildlife.ogg",
+      },
+      thumb: "/images/wildlife.jpg",
+      type: "upcoming",
+      info: {
+        title: "Wild Horses",
+        cast: "Mick Jagger, Brian Jones, Keith Richards, Bill Wyman",
+        director: "Jimmy Miller",
+        duration: 46,
+      },
+    },
+    {
+      id: 8,
+      sources: {
+        mp4: "https://courses.cs.cityu.edu.hk/cs2204/video/wonders.mp4",
+        ogg: "https://courses.cs.cityu.edu.hk/cs2204/video/wonders.ogg",
+      },
+      thumb: "/images/wonders.jpg",
+      type: "upcoming",
+      info: {
+        title: "Mad Max: Fury Road",
+        cast: "Tom Hardy, Charlize Theron, Nicholas Hoult",
+        director: "George Miller",
+        duration: 120,
+      },
+    },
+  ];
 }
